10c159775efbc93ab3b1b6962022d543614304b9
Clebio de Oliveira Junior <clebiojuniorcj@gmail.com>
2022-03-28 18:35:29 -0300

    added alpha default value

--- statistical_tests.py
+++ statistical_tests.py
@@ -6,34 +6,31 @@ from scipy import stats
 """
 Statistical tests to check whether a distribution is normal.
 """
-def dagostino_test(data):
+def dagostino_test(data, alpha=0.05):
     
-    alpha = 0.05
     _, p = stats.normaltest(data)
     result = 'Not normal' if p < alpha else 'Normal'
 
     return result
 
 
-def kurtosis_test(data):
+def kurtosis_test(data, alpha=0.05):
     
-    alpha = 0.05
     _, p = stats.kurtosistest(data)
     result = 'Not normal' if p < alpha else 'Normal'
     
     return f'{result}, {round(p, 4)}'
 
 
-def skewness_test(data):
+def skewness_test(data, alpha=0.05):
     
-    alpha = 0.05
     _, p = stats.skewtest(data)
     result = 'Not normal' if p < alpha else 'Normal'
     
     return f'{result}, {round(p, 4)}'
 
 
-def normal_tests(data):
+def normal_tests(data, alpha=0.05):
     
     yj = stats.yeojohnson(data)[0]
     sq = np.sqrt(data)
@@ -42,9 +39,8 @@ def normal_tests(data):
     
     return pd.DataFrame({
         'Skewness': [stats.skew(data), stats.skew(sq), stats.skew(cb), stats.skew(log), stats.skew(yj)],
-        'Skewness Test': [skewness_test(data), skewness_test(sq), skewness_test(cb), skewness_test(log), skewness_test(yj)],
+        'Skewness Test': [skewness_test(data, alpha=alpha), skewness_test(sq, alpha=alpha), skewness_test(cb, alpha=alpha), skewness_test(log, alpha=alpha), skewness_test(yj, alpha=alpha)],
         'Kurtosis': [stats.kurtosis(data), stats.kurtosis(sq), stats.kurtosis(cb), stats.kurtosis(log), stats.kurtosis(yj)],
-        'Kurtosis Test': [kurtosis_test(data), kurtosis_test(sq), kurtosis_test(cb), kurtosis_test(log), kurtosis_test(yj)],
-        'Normal Test': [dagostino_test(data), dagostino_test(sq), dagostino_test(cb), dagostino_test(log), dagostino_test(yj)]}, 
+        'Kurtosis Test': [kurtosis_test(data, alpha=alpha), kurtosis_test(sq, alpha=alpha), kurtosis_test(cb, alpha=alpha), kurtosis_test(log, alpha=alpha), kurtosis_test(yj, alpha=alpha)],
+        'Normal Test': [dagostino_test(data, alpha=alpha), dagostino_test(sq, alpha=alpha), dagostino_test(cb, alpha=alpha), dagostino_test(log, alpha=alpha), dagostino_test(yj, alpha=alpha)]}, 
         index=['default', 'sqrt', 'cuberoot', 'log', 'yeojohnson'])
-
