28a21217d8636bab3537941623bb3c50d627528a
Clébio de Oliveira Júnior <clebiojuniorcj@gmail.com>
2023-10-29 22:48:55 -0300

    Update statistical_tests.py

--- statistical_tests.py
+++ statistical_tests.py
@@ -8,29 +8,29 @@ from scipy import stats
 Statistical tests to check whether a distribution is normal.
 """
 def apply_dagostino_test(variable, alpha=0.05, return_p=True):
-
+    
     _, p = stats.normaltest(variable)
     
     result = 'Not normal' if p < alpha else 'Normal'
-
+    
     return (result, p) if return_p else result
 
 
 def apply_kurtosis_test(variable, alpha=0.05, return_p=True):
-
+    
     _, p = stats.kurtosistest(variable)
     
     result = 'Not normal' if p < alpha else 'Normal'
-
+    
     return (result, p) if return_p else result
 
 
 def apply_skewness_test(variable, alpha=0.05, return_p=True):
-
+    
     _, p = stats.skewtest(variable)
     
     result = 'Not normal' if p < alpha else 'Normal'
-
+    
     return (result, p) if return_p else result
 
 
@@ -46,8 +46,7 @@ def apply_normal_tests(variable, alpha=0.05, return_p=False, return_kurt_skew=Fa
         'Sqrt': apply_dagostino_test(sq, alpha=alpha, return_p=return_p),
         'Cube Root': apply_dagostino_test(cb, alpha=alpha, return_p=return_p),
         'Log': apply_dagostino_test(log, alpha=alpha, return_p=return_p),
-        'Yeo Johnson': apply_dagostino_test(yj, alpha=alpha, return_p=return_p)
-    }
+        'Yeo Johnson': apply_dagostino_test(yj, alpha=alpha, return_p=return_p)}
     
     dict_result = {'Normal Tests': dict_normal_tests}
     
@@ -58,18 +57,16 @@ def apply_normal_tests(variable, alpha=0.05, return_p=False, return_kurt_skew=Fa
             'Sqrt': apply_skewness_test(sq, alpha=alpha, return_p=return_p),
             'Cube Root': apply_skewness_test(cb, alpha=alpha, return_p=return_p),
             'Log': apply_skewness_test(log, alpha=alpha, return_p=return_p),
-            'Yeo Johnson': apply_skewness_test(yj, alpha=alpha, return_p=return_p)
-        }
-
+            'Yeo Johnson': apply_skewness_test(yj, alpha=alpha, return_p=return_p)}
+        
         dict_kurtosis_tests = {
             'Original': apply_kurtosis_test(variable, alpha=alpha, return_p=return_p),
             'Sqrt': apply_kurtosis_test(sq, alpha=alpha, return_p=return_p),
             'Cube Root': apply_kurtosis_test(cb, alpha=alpha, return_p=return_p),
             'Log': apply_kurtosis_test(log, alpha=alpha, return_p=return_p),
-            'Yeo Johnson': apply_kurtosis_test(yj, alpha=alpha, return_p=return_p)
-        }
-    
+            'Yeo Johnson': apply_kurtosis_test(yj, alpha=alpha, return_p=return_p)}
+        
         dict_result.update(
             {'Skewness Tests': dict_skewness_tests, 'Kurtosis Tests': dict_kurtosis_tests})
-
+        
     return dict_result
