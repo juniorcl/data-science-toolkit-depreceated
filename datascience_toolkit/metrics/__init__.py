eedbdd37258a935f1510b06fe847b79aa4d4b00d
Clébio de Oliveira Júnior <clebiojuniorcj@gmail.com>
2024-02-24 23:05:54 -0300

    renamed the folder

--- datascience_toolkit/metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-from .top_k.top_k_recall    import top_k_recall
-from .top_k.top_k_f1score   import top_k_f1score
-from .top_k.top_k_precision import top_k_precision
-
-__all__ = [
-    "top_k_recall",
-    "top_k_f1score",
-    "top_k_precision"
-]
--- datascience_toolkit/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-from .transformations.gaussian_transfomation import GaussianTransformer
-
-__all__ = [
-    "GaussianTransformer"
-]
