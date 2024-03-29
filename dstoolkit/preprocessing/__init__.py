4e764700e02d17e0341983ba39ba617c5eb0d0e4
Clébio de Oliveira Júnior <clebiojuniorcj@gmail.com>
2024-03-29 11:30:09 -0300

    added agg_cat

--- dstoolkit/preprocessing/__init__.py
+++ dstoolkit/preprocessing/__init__.py
@@ -1,7 +1,9 @@
-from .aggregations.numerical_aggregations    import agg_num
+from .aggregations.numerical_aggregation     import agg_num
+from .aggregations.categorical_aggregation   import agg_cat
 from .transformations.gaussian_transfomation import GaussianTransformer
 
 __all__ = [
     "GaussianTransformer",
-    "agg_num"
+    "agg_num",
+    "agg_cat"
 ]
